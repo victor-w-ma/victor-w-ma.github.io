7e1e594082e7e45ebc55af511675eafb0e1ee532
Victor Ma <victorwma@outlook.com>
2023-02-15 22:09:48 -0800

    Remove commented code in summarize.py

--- _posts/summarize.py
+++ _posts/summarize.py
@@ -1,29 +1,7 @@
 import sys
 
-
-# replace_dict = {}
-# with open('dict.txt') as dict_file:
-#     for line in dict_file:
-#         old_word, new_word = line[:-1].split(' ')
-#         replace_dict[old_word] = new_word
-
 path = sys.argv[1]
 
-# new_lines = []
-# with open(path) as file_:
-#     for i, line in enumerate(file_):
-#         if i % 1000 == 0:
-#             print(f'{i}')
-#         line = line[:-1]
-#         for old_word, new_word in replace_dict.items():
-#             line = line.replace(old_word, new_word)
-#         new_lines.append(line)
-# with open('new.md', 'w') as file_:
-#     for i, new_line in enumerate(new_lines):
-#         if i % 1000 == 0:
-#             print(f'{i}')
-#         print(new_line, file=file_)
-# exit(0)
 with open(path) as file_:
     for line in file_:
         if line.startswith('##'):
@@ -37,4 +15,3 @@ with open(path) as file_:
             link = heading.replace('-', '').replace(' ', '-').replace('、', '')
             toc_item = f'{" " * (head_level - 1) * 4}- [{heading}](#{link})'
             print(toc_item)
-        
